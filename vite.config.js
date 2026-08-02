053ee83609e063455c3f8a3525aacfacdb13b9e8
Shahzaib-Maqbool <shahzaib405022@gmail.com>
2026-08-02 06:47:27 +0500

    Fix Vercel React Router refresh

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
   base: '/',
-  
   build: {
     outDir: 'dist',
     sourcemap: false,
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  
-  server: {
-    historyApiFallback: true,  
   },
 })
